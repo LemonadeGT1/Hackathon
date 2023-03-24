5460d9b561d36b7da0c1c46f3ae25c75037fba55
iangrell <igrell27@outlook.com>
2023-03-24 10:34:55 -0600

    topic create

--- server/controllers/TopicsController.js
+++ server/controllers/TopicsController.js
@@ -1,3 +1,4 @@
+import { topicsService } from "../services/TopicsService.js";
 import BaseController from "../utils/BaseController.js";
 
 
@@ -7,6 +8,16 @@ export class TopicsController extends BaseController {
     constructor() {
         super('api/topics')
         this.router
-        
+        .post('', this.createTopic)
+    }
+
+    async createTopic(req, res, next) {
+        try {
+            const topicData = req.body
+            const newTopic = await topicsService.createTopic(topicData)
+            res.send(newTopic)
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- server/services/TopicsService.js
+++ server/services/TopicsService.js
@@ -1,8 +1,14 @@
+import { dbContext } from "../db/DbContext.js"
 
 
 
 
 class TopicsService{
+
+    async createTopic(topicData) {
+        const newTopic = await dbContext.Topics.create(topicData)
+        return newTopic
+    }
 
 }
 
