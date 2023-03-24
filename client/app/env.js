994b9aa3dd5568f29961f0bdbb40585118e01b8e
alexmquan <alexmquan93@gmail.com>
2023-03-24 12:15:26 -0600

    env.js fix

--- client/app/env.js
+++ client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-v3bz7ala46wwesk6.us.auth0.com'
+export const audience = 'http://BCWSandbox.io'
+export const clientId = 'VI2WvrMVvaOvTIhEcAk6Cu913MBB3FIh'
