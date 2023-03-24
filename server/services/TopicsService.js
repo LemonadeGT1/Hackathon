90ef8e1a638529d632b65ebb01936402550f7c03
iangrell <igrell27@outlook.com>
2023-03-24 11:03:02 -0600

    get topics

--- server/services/TopicsService.js
+++ server/services/TopicsService.js
@@ -4,12 +4,16 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class TopicsService{
-
+    
     async createTopic(topicData) {
         const newTopic = await dbContext.Topics.create(topicData)
         return newTopic
     }
-
+    
+    async getTopics(query) {
+        const topics = await dbContext.Topics.find(query)
+        return topics
+    }
 }
 
 export const topicsService = new TopicsService()
