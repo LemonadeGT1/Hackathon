228d8b9d24d0adf03652328db27aef824a53ab28
Isaac Duff <4mustangid@gmail.com>
2023-03-24 17:18:00 -0600

    cruds done

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -4,19 +4,34 @@ import { BadRequest, Forbidden } from "../utils/Errors.js"
 
 
 class CommentsService {
-  getCommentById(commentId) {
-    throw new Error("Method not implemented.")
-  }
+
   async getComments(query) {
     const comments = await dbContext.Comments.find(query)
     return comments
   }
+
+
 
   async createComment(commentData) {
     const newComment = await dbContext.Comments.create(commentData)
     // await newComment.populate('author', 'name picture')
     return newComment
   }
+
+
+
+  async editComment(commentEdits, commentId) {
+    const originalComment = await dbContext.Comments.findById(commentId)
+    if (!originalComment) {
+      throw new BadRequest("Cannot Edit Nonexistent Comment")
+    } else {
+      originalComment.body = commentEdits.body ? commentEdits.body : originalComment.body
+      await originalComment.save()
+      return originalComment
+    }
+  }
+
+
 
   async deleteComment(userId, commentId) {
     const foundComment = await dbContext.Comments.findById(commentId)
